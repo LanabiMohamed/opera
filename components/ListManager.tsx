75fddfa831d5616442380bcf40dd864506029e68
medlnb <mohamedlanabi0@gmail.com>
2024-09-29 15:56:00 +0100

    imports

--- components/ListManager.tsx
+++ components/ListManager.tsx
@@ -1,7 +1,6 @@
 import { useState } from "react";
 import { IoIosAddCircleOutline } from "react-icons/io";
 import { MdOutlineDelete } from "react-icons/md";
-import { notify } from "./Sonner";
 
 function ListManager({
   title,
